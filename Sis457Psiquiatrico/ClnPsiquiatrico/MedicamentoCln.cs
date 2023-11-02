8e9ac252ab8bc8a9999df15f8e3ef40570f04544
fer77vaca <ferchi77vaca@gmail.com>
2023-11-02 16:55:08 -0400

    Se creo el proyecto ClnPsiquiatrico, y las 5 clases Cln.cs de la base de datos

---
+++ Sis457Psiquiatrico/ClnPsiquiatrico/MedicamentoCln.cs
@@ -0,0 +1,71 @@
+﻿using CadPsiquiatrico;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClnPsiquiatrico
+{
+    public class MedicamentoCln
+    {
+        public static int insertar (Medicamento medicamento)
+        {
+            using (var context = new LabPsiquiatricoEntities())
+            {
+                context.Medicamento.Add(medicamento);
+                context.SaveChanges();
+                return medicamento.id;
+            }
+        }
+        public static int actualizar (Medicamento medicamento)
+        {
+            using (var context = new LabPsiquiatricoEntities())
+            {
+                var existente = context.Medicamento.Find (medicamento.id);
+                existente.nombreMedicamento = medicamento.nombreMedicamento;
+                existente.dosificacion = medicamento.dosificacion;
+                existente.precio = medicamento.precio;
+                existente.usuarioRegistro = medicamento.usuarioRegistro;
+                return context.SaveChanges();
+            }
+        }
+        public static int eliminar (int id, string usuarioRegistro)
+        {
+            using (var context = new LabPsiquiatricoEntities())
+            {
+                var existente = context.Medicamento.Find(id);
+                existente.estado = -1;
+                existente.usuarioRegistro = usuarioRegistro;
+                return context.SaveChanges();
+            }
+        }
+
+        // OBTENER POR UN id
+        public static Medicamento get (int id)
+        {
+            using (var context = new LabPsiquiatricoEntities())
+            {
+                return context.Medicamento.Find(id);
+            }
+        }
+
+        // OBTENER TODOS LISTA
+        public static List<Medicamento> listar()
+        {
+            using (var context = new LabPsiquiatricoEntities())
+            {
+                return context.Medicamento.Where(x => x.estado != -1).ToList();
+            }
+        }
+
+        // OBTENER TODOS CON PROCEDIMIENTO ALMACENADO
+        public static List<paMedicamentoListar_Result> listarPa(string parametro)
+        {
+            using (var context = new LabPsiquiatricoEntities())
+            {
+                return context.paMedicamentoListar(parametro).ToList();
+            }
+        }
+    }
+}
---
+++ Sis457Psiquiatrico/ClnPsiquiatrico/PacienteCln.cs
@@ -0,0 +1,82 @@
+﻿using CadPsiquiatrico;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClnPsiquiatrico
+{
+    public class PacienteCln
+    {
+        public static int insertar (Paciente paciente)
+        {
+            using (var context = new LabPsiquiatricoEntities())
+            {
+                context.Paciente.Add (paciente);
+                context.SaveChanges();
+                return paciente.id;
+            }
+        }
+        public static int actualizar (Paciente paciente)
+        {
+            using (var context = new LabPsiquiatricoEntities())
+            {
+                var existente = context.Paciente.Find(paciente.id);
+                existente.nombre = paciente.nombre;
+                existente.apellido = paciente.apellido;
+                existente.cedulaIdentidad = paciente.cedulaIdentidad;
+                existente.razonSocial = paciente.razonSocial;
+                existente.fechaNacimiento = paciente.fechaNacimiento;
+                existente.edad = paciente.edad;
+                existente.genero = paciente.genero;
+                existente.direccion = paciente.direccion;
+                existente.telefono = paciente.telefono;
+                existente.historialMedico = paciente.historialMedico;
+                existente.fechaAdmision = paciente.fechaAdmision;
+                existente.fechaAlta = paciente.fechaAlta;
+                existente.tratamiento = paciente.tratamiento;
+                existente.usuarioRegistro = paciente.usuarioRegistro;
+                return context.SaveChanges();
+            }
+        }
+        public static int eliminar(int id, string usuarioRegistro)
+        {
+            using (var context = new LabPsiquiatricoEntities())
+            {
+                var existente = context.Paciente.Find(id);
+                existente.estado = -1;
+                existente.usuarioRegistro = usuarioRegistro;
+                return context.SaveChanges();
+            }
+        }
+
+        // OBTENER POR UN id
+        public static Paciente get(int id)
+        {
+            using (var context = new LabPsiquiatricoEntities())
+            {
+                return context.Paciente.Find(id);
+            }
+        }
+
+        // OBTENER TODOS LISTA
+        public static List<Paciente> listar()
+        {
+            using (var context = new LabPsiquiatricoEntities())
+            {
+                return context.Paciente.Where(x => x.estado != -1).ToList();
+            }
+        }
+
+        // OBTENER TODOS CON PROCEDIMIENTO ALMACENADO
+        public static List<paPacienteListar_Result> listarPa(string parametro)
+        {
+            using (var context = new LabPsiquiatricoEntities())
+            {
+                return context.paPacienteListar(parametro).ToList();
+            }
+        }
+    }
+}
