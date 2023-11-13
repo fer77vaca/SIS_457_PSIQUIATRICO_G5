aefb703e9a302d050c45bf0816b5d5a3c0f33d0e
fer77vaca <ferchi77vaca@gmail.com>
2023-11-13 10:29:51 -0400

    FrmPaciente con datos relacionantes a Personal boton Buscar y Guardar

--- Sis457Psiquiatrico/CpPsiquiatrico/FrmPaciente.Designer.cs
+++ Sis457Psiquiatrico/CpPsiquiatrico/FrmPaciente.Designer.cs
@@ -40,32 +40,45 @@
             this.btnEditar = new System.Windows.Forms.Button();
             this.btnNuevo = new System.Windows.Forms.Button();
             this.gbxDatos = new System.Windows.Forms.GroupBox();
-            this.txtHorario = new System.Windows.Forms.TextBox();
-            this.txtEspecialidad = new System.Windows.Forms.TextBox();
+            this.nudEdad = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpFechaAdmision = new System.Windows.Forms.DateTimePicker();
+            this.lblFechaAdmision = new System.Windows.Forms.Label();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.txtTratamiento = new System.Windows.Forms.TextBox();
+            this.lblTratamiento = new System.Windows.Forms.Label();
+            this.txtHistorial = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
             this.txtCedula = new System.Windows.Forms.TextBox();
             this.txtNombre = new System.Windows.Forms.TextBox();
             this.lblHistorial = new System.Windows.Forms.Label();
             this.lblTelefono = new System.Windows.Forms.Label();
             this.lblCedula = new System.Windows.Forms.Label();
             this.lblNombre = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.lblTratamiento = new System.Windows.Forms.Label();
-            this.btnCancelar = new System.Windows.Forms.Button();
-            this.btnGuardar = new System.Windows.Forms.Button();
+            this.cbxPersonal = new System.Windows.Forms.ComboBox();
+            this.lblPersonalAsignado = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).BeginInit();
             this.gbxLista.SuspendLayout();
             this.pnlAcciones.SuspendLayout();
             this.gbxDatos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudEdad)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvLista
             // 
+            this.dgvLista.AllowUserToAddRows = false;
+            this.dgvLista.AllowUserToDeleteRows = false;
+            this.dgvLista.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.dgvLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvLista.Location = new System.Drawing.Point(7, 27);
-            this.dgvLista.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvLista.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvLista.MultiSelect = false;
             this.dgvLista.Name = "dgvLista";
+            this.dgvLista.ReadOnly = true;
             this.dgvLista.RowHeadersWidth = 51;
             this.dgvLista.RowTemplate.Height = 24;
+            this.dgvLista.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgvLista.Size = new System.Drawing.Size(962, 121);
             this.dgvLista.TabIndex = 0;
             // 
@@ -77,6 +90,7 @@
             this.btnBuscar.TabIndex = 8;
             this.btnBuscar.Text = "Buscar";
             this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
             // lblBusqueda
             // 
@@ -93,6 +107,7 @@
             this.txtParametro.Name = "txtParametro";
             this.txtParametro.Size = new System.Drawing.Size(787, 27);
             this.txtParametro.TabIndex = 6;
+            this.txtParametro.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtParametro_KeyPress);
             // 
             // lblTitulo
             // 
@@ -134,6 +149,7 @@
             this.btnCerrar.TabIndex = 14;
             this.btnCerrar.Text = "Cerrar";
             this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
             // 
             // btnEliminar
             // 
@@ -154,6 +170,7 @@
             this.btnEditar.TabIndex = 12;
             this.btnEditar.Text = "Editar";
             this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
             // 
             // btnNuevo
             // 
@@ -163,15 +180,22 @@
             this.btnNuevo.TabIndex = 11;
             this.btnNuevo.Text = "Nuevo";
             this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
             // 
             // gbxDatos
             // 
+            this.gbxDatos.Controls.Add(this.lblPersonalAsignado);
+            this.gbxDatos.Controls.Add(this.cbxPersonal);
+            this.gbxDatos.Controls.Add(this.nudEdad);
+            this.gbxDatos.Controls.Add(this.label1);
+            this.gbxDatos.Controls.Add(this.dtpFechaAdmision);
+            this.gbxDatos.Controls.Add(this.lblFechaAdmision);
             this.gbxDatos.Controls.Add(this.btnCancelar);
             this.gbxDatos.Controls.Add(this.btnGuardar);
-            this.gbxDatos.Controls.Add(this.textBox1);
+            this.gbxDatos.Controls.Add(this.txtTratamiento);
             this.gbxDatos.Controls.Add(this.lblTratamiento);
-            this.gbxDatos.Controls.Add(this.txtHorario);
-            this.gbxDatos.Controls.Add(this.txtEspecialidad);
+            this.gbxDatos.Controls.Add(this.txtHistorial);
+            this.gbxDatos.Controls.Add(this.txtTelefono);
             this.gbxDatos.Controls.Add(this.txtCedula);
             this.gbxDatos.Controls.Add(this.txtNombre);
             this.gbxDatos.Controls.Add(this.lblHistorial);
@@ -185,19 +209,89 @@
             this.gbxDatos.TabStop = false;
             this.gbxDatos.Text = "Datos del Paciente";
             // 
-            // txtHorario
+            // nudEdad
             // 
-            this.txtHorario.Location = new System.Drawing.Point(140, 137);
-            this.txtHorario.Name = "txtHorario";
-            this.txtHorario.Size = new System.Drawing.Size(264, 27);
-            this.txtHorario.TabIndex = 27;
+            this.nudEdad.Location = new System.Drawing.Point(609, 70);
+            this.nudEdad.Name = "nudEdad";
+            this.nudEdad.Size = new System.Drawing.Size(120, 27);
+            this.nudEdad.TabIndex = 35;
             // 
-            // txtEspecialidad
+            // label1
             // 
-            this.txtEspecialidad.Location = new System.Drawing.Point(140, 101);
-            this.txtEspecialidad.Name = "txtEspecialidad";
-            this.txtEspecialidad.Size = new System.Drawing.Size(264, 27);
-            this.txtEspecialidad.TabIndex = 26;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(544, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Edad:";
+            // 
+            // dtpFechaAdmision
+            // 
+            this.dtpFechaAdmision.Location = new System.Drawing.Point(609, 29);
+            this.dtpFechaAdmision.Name = "dtpFechaAdmision";
+            this.dtpFechaAdmision.Size = new System.Drawing.Size(301, 27);
+            this.dtpFechaAdmision.TabIndex = 33;
+            // 
+            // lblFechaAdmision
+            // 
+            this.lblFechaAdmision.AutoSize = true;
+            this.lblFechaAdmision.Location = new System.Drawing.Point(462, 32);
+            this.lblFechaAdmision.Name = "lblFechaAdmision";
+            this.lblFechaAdmision.Size = new System.Drawing.Size(134, 20);
+            this.lblFechaAdmision.TabIndex = 32;
+            this.lblFechaAdmision.Text = "Fecha Admision:";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(882, 182);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(87, 32);
+            this.btnCancelar.TabIndex = 31;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(785, 182);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(91, 32);
+            this.btnGuardar.TabIndex = 30;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // txtTratamiento
+            // 
+            this.txtTratamiento.BackColor = System.Drawing.SystemColors.Info;
+            this.txtTratamiento.Location = new System.Drawing.Point(140, 170);
+            this.txtTratamiento.Name = "txtTratamiento";
+            this.txtTratamiento.Size = new System.Drawing.Size(589, 27);
+            this.txtTratamiento.TabIndex = 29;
+            // 
+            // lblTratamiento
+            // 
+            this.lblTratamiento.AutoSize = true;
+            this.lblTratamiento.Location = new System.Drawing.Point(6, 170);
+            this.lblTratamiento.Name = "lblTratamiento";
+            this.lblTratamiento.Size = new System.Drawing.Size(103, 20);
+            this.lblTratamiento.TabIndex = 28;
+            this.lblTratamiento.Text = "Tratamiento:";
+            // 
+            // txtHistorial
+            // 
+            this.txtHistorial.BackColor = System.Drawing.SystemColors.Info;
+            this.txtHistorial.Location = new System.Drawing.Point(140, 137);
+            this.txtHistorial.Name = "txtHistorial";
+            this.txtHistorial.Size = new System.Drawing.Size(264, 27);
+            this.txtHistorial.TabIndex = 27;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(140, 101);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(264, 27);
+            this.txtTelefono.TabIndex = 26;
             // 
             // txtCedula
             // 
@@ -249,46 +343,29 @@
             this.lblNombre.TabIndex = 20;
             this.lblNombre.Text = "Nombre:";
             // 
-            // textBox1
+            // cbxPersonal
             // 
-            this.textBox1.Location = new System.Drawing.Point(140, 170);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(264, 27);
-            this.textBox1.TabIndex = 29;
+            this.cbxPersonal.FormattingEnabled = true;
+            this.cbxPersonal.Location = new System.Drawing.Point(609, 109);
+            this.cbxPersonal.Name = "cbxPersonal";
+            this.cbxPersonal.Size = new System.Drawing.Size(301, 28);
+            this.cbxPersonal.TabIndex = 36;
             // 
-            // lblTratamiento
+            // lblPersonalAsignado
             // 
-            this.lblTratamiento.AutoSize = true;
-            this.lblTratamiento.Location = new System.Drawing.Point(6, 170);
-            this.lblTratamiento.Name = "lblTratamiento";
-            this.lblTratamiento.Size = new System.Drawing.Size(103, 20);
-            this.lblTratamiento.TabIndex = 28;
-            this.lblTratamiento.Text = "Tratamiento:";
-            // 
-            // btnCancelar
-            // 
-            this.btnCancelar.Location = new System.Drawing.Point(883, 182);
-            this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(87, 32);
-            this.btnCancelar.TabIndex = 31;
-            this.btnCancelar.Text = "Cancelar";
-            this.btnCancelar.UseVisualStyleBackColor = true;
-            // 
-            // btnGuardar
-            // 
-            this.btnGuardar.Location = new System.Drawing.Point(786, 182);
-            this.btnGuardar.Name = "btnGuardar";
-            this.btnGuardar.Size = new System.Drawing.Size(91, 32);
-            this.btnGuardar.TabIndex = 30;
-            this.btnGuardar.Text = "Guardar";
-            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.lblPersonalAsignado.AutoSize = true;
+            this.lblPersonalAsignado.Location = new System.Drawing.Point(442, 112);
+            this.lblPersonalAsignado.Name = "lblPersonalAsignado";
+            this.lblPersonalAsignado.Size = new System.Drawing.Size(154, 20);
+            this.lblPersonalAsignado.TabIndex = 37;
+            this.lblPersonalAsignado.Text = "Personal Asignado:";
             // 
             // FrmPaciente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSalmon;
-            this.ClientSize = new System.Drawing.Size(1000, 564);
+            this.ClientSize = new System.Drawing.Size(1000, 559);
             this.Controls.Add(this.gbxDatos);
             this.Controls.Add(this.pnlAcciones);
             this.Controls.Add(this.gbxLista);
@@ -297,14 +374,16 @@
             this.Controls.Add(this.txtParametro);
             this.Controls.Add(this.lblTitulo);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FrmPaciente";
             this.Text = "FrmPaciente";
+            this.Load += new System.EventHandler(this.FrmPaciente_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).EndInit();
             this.gbxLista.ResumeLayout(false);
             this.pnlAcciones.ResumeLayout(false);
             this.gbxDatos.ResumeLayout(false);
             this.gbxDatos.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudEdad)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -324,17 +403,23 @@
         private System.Windows.Forms.Button btnEditar;
         private System.Windows.Forms.Button btnNuevo;
         private System.Windows.Forms.GroupBox gbxDatos;
-        private System.Windows.Forms.TextBox txtHorario;
-        private System.Windows.Forms.TextBox txtEspecialidad;
+        private System.Windows.Forms.TextBox txtHistorial;
+        private System.Windows.Forms.TextBox txtTelefono;
         private System.Windows.Forms.TextBox txtCedula;
         private System.Windows.Forms.TextBox txtNombre;
         private System.Windows.Forms.Label lblHistorial;
         private System.Windows.Forms.Label lblTelefono;
         private System.Windows.Forms.Label lblCedula;
         private System.Windows.Forms.Label lblNombre;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtTratamiento;
         private System.Windows.Forms.Label lblTratamiento;
         private System.Windows.Forms.Button btnCancelar;
         private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.NumericUpDown nudEdad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpFechaAdmision;
+        private System.Windows.Forms.Label lblFechaAdmision;
+        private System.Windows.Forms.ComboBox cbxPersonal;
+        private System.Windows.Forms.Label lblPersonalAsignado;
     }
 }
--- Sis457Psiquiatrico/CpPsiquiatrico/FrmPaciente.cs
+++ Sis457Psiquiatrico/CpPsiquiatrico/FrmPaciente.cs
@@ -1,4 +1,5 @@
-﻿using ClnPsiquiatrico;
+﻿using CadPsiquiatrico;
+using ClnPsiquiatrico;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,10 +14,109 @@ namespace CpPsiquiatrico
 {
     public partial class FrmPaciente : Form
     {
+        bool esNuevo = false;
         public FrmPaciente()
         {
             InitializeComponent();
-            dgvLista.DataSource = PacienteCln.listarPa("");
+        }
+        private void listar()
+        {
+            var pacientes = PacienteCln.listarPa(txtParametro.Text.Trim());
+            dgvLista.DataSource = pacientes;
+            dgvLista.Columns["id"].Visible = false;
+            dgvLista.Columns["idPersonal"].Visible = false;
+            dgvLista.Columns["estado"].Visible = false;
+            dgvLista.Columns["nombrePersonal"].HeaderText = "Personal";
+            dgvLista.Columns["nombrePaciente"].HeaderText = "Paciente";
+            dgvLista.Columns["cedulaIdentidad"].HeaderText = "C.I.";
+            dgvLista.Columns["edad"].HeaderText = "Edad";
+            dgvLista.Columns["telefono"].HeaderText = "Telefono";
+            dgvLista.Columns["historialMedico"].HeaderText = "Historial_Medico";
+            dgvLista.Columns["tratamiento"].HeaderText = "Tratamiento";
+            dgvLista.Columns["fechaAdmision"].HeaderText = "Fecha_Admision";
+            dgvLista.Columns["usuarioRegistro"].HeaderText = "Usuario";
+            dgvLista.Columns["fechaRegistro"].HeaderText = "Fecha Registro";
+            btnEditar.Enabled = pacientes.Count>0;
+            btnEliminar.Enabled = pacientes.Count>0;
+            if (pacientes.Count > 0) dgvLista.Rows[0].Cells["nombrePaciente"].Selected = true;
+        }
+        private void cargarPersonal ()
+        {
+            cbxPersonal.DataSource = PersonalCln.listar();
+            cbxPersonal.DisplayMember = "nombre"; 
+            cbxPersonal.ValueMember = "id";
+        }
+
+        private void FrmPaciente_Load(object sender, EventArgs e)
+        {
+            Size = new Size(1018, 374);
+            listar();
+            cargarPersonal();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            esNuevo = true;
+            Size = new Size(1018, 606);
+            txtNombre.Focus();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            esNuevo = false;
+            Size = new Size(1018, 606);
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Size = new Size(1018, 374);
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            listar();
+        }
+
+        private void txtParametro_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter) listar();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            var paciente = new Paciente();
+            paciente.nombre = txtNombre.Text.Trim();
+            paciente.cedulaIdentidad = txtCedula.Text.Trim();
+            paciente.telefono = txtTelefono.Text.Trim();
+            paciente.historialMedico = txtHistorial.Text.Trim();
+            paciente.tratamiento = txtTratamiento.Text.Trim();
+            paciente.edad = Convert.ToInt32(nudEdad.Text);
+            paciente.fechaAdmision = dtpFechaAdmision.Value;
+            paciente.usuarioRegistro = "FVC";
+
+            if (esNuevo)
+            {
+                paciente.fechaRegistro = DateTime.Now;
+                paciente.estado = 1;
+                paciente.idPersonal = Convert.ToInt32(cbxPersonal.SelectedValue);
+                //paciente.idPersonal = 2;
+                PacienteCln.insertar(paciente);
+            }
+            else
+            {
+                int index = dgvLista.CurrentCell.RowIndex;
+                paciente.id = Convert.ToInt32(dgvLista.Rows[index].Cells["id"].Value);
+                PacienteCln.actualizar(paciente);
+            }
+            listar();
+            btnCancelar.PerformClick();
+            MessageBox.Show("Paciente guardado exitosamente", "::: Psiquiatrico - Mensaje :::",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
