e7bbc6501d1822c3d4f98d337a3d274c406b214b
fer77vaca <ferchi77vaca@gmail.com>
2023-11-18 05:11:20 -0400

    Implmentacion cln paciente

--- Sis457Psiquiatrico/CpPsiquiatrico/FrmCita.Designer.cs
+++ Sis457Psiquiatrico/CpPsiquiatrico/FrmCita.Designer.cs
@@ -29,34 +29,309 @@
         private void InitializeComponent()
         {
             this.dgvLista = new System.Windows.Forms.DataGridView();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.lblBusqueda = new System.Windows.Forms.Label();
+            this.txtParametro = new System.Windows.Forms.TextBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.pnlAcciones = new System.Windows.Forms.Panel();
+            this.gbxLista = new System.Windows.Forms.GroupBox();
+            this.gbxDatos = new System.Windows.Forms.GroupBox();
+            this.nudPago = new System.Windows.Forms.NumericUpDown();
+            this.lblPago = new System.Windows.Forms.Label();
+            this.dtpFechaResrvacion = new System.Windows.Forms.DateTimePicker();
+            this.lblFechaReservacion = new System.Windows.Forms.Label();
+            this.txtMotivo = new System.Windows.Forms.TextBox();
+            this.lblMotivo = new System.Windows.Forms.Label();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.cbxPaciente = new System.Windows.Forms.ComboBox();
+            this.lblPaciente = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).BeginInit();
+            this.pnlAcciones.SuspendLayout();
+            this.gbxLista.SuspendLayout();
+            this.gbxDatos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPago)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvLista
             // 
+            this.dgvLista.AllowUserToAddRows = false;
+            this.dgvLista.AllowUserToDeleteRows = false;
+            this.dgvLista.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.dgvLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvLista.Location = new System.Drawing.Point(92, 131);
+            this.dgvLista.Location = new System.Drawing.Point(7, 27);
+            this.dgvLista.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvLista.MultiSelect = false;
             this.dgvLista.Name = "dgvLista";
+            this.dgvLista.ReadOnly = true;
             this.dgvLista.RowHeadersWidth = 51;
             this.dgvLista.RowTemplate.Height = 24;
-            this.dgvLista.Size = new System.Drawing.Size(595, 237);
+            this.dgvLista.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvLista.Size = new System.Drawing.Size(947, 134);
             this.dgvLista.TabIndex = 0;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(842, 63);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(77, 32);
+            this.btnBuscar.TabIndex = 14;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // lblBusqueda
+            // 
+            this.lblBusqueda.AutoSize = true;
+            this.lblBusqueda.Location = new System.Drawing.Point(27, 45);
+            this.lblBusqueda.Name = "lblBusqueda";
+            this.lblBusqueda.Size = new System.Drawing.Size(227, 20);
+            this.lblBusqueda.TabIndex = 13;
+            this.lblBusqueda.Text = "Buscar por motivo de la Cita:";
+            // 
+            // txtParametro
+            // 
+            this.txtParametro.Location = new System.Drawing.Point(31, 68);
+            this.txtParametro.Name = "txtParametro";
+            this.txtParametro.Size = new System.Drawing.Size(787, 27);
+            this.txtParametro.TabIndex = 12;
+            this.txtParametro.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtParametro_KeyPress);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(405, 9);
+            this.lblTitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(161, 42);
+            this.lblTitulo.TabIndex = 11;
+            this.lblTitulo.Text = "Cita";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(512, 3);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(77, 32);
+            this.btnCerrar.TabIndex = 14;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Enabled = false;
+            this.btnEliminar.Location = new System.Drawing.Point(394, 3);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(87, 32);
+            this.btnEliminar.TabIndex = 13;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Enabled = false;
+            this.btnEditar.Location = new System.Drawing.Point(280, 3);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(77, 32);
+            this.btnEditar.TabIndex = 12;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(171, 3);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(77, 32);
+            this.btnNuevo.TabIndex = 11;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // pnlAcciones
+            // 
+            this.pnlAcciones.Controls.Add(this.btnCerrar);
+            this.pnlAcciones.Controls.Add(this.btnEliminar);
+            this.pnlAcciones.Controls.Add(this.btnEditar);
+            this.pnlAcciones.Controls.Add(this.btnNuevo);
+            this.pnlAcciones.Location = new System.Drawing.Point(121, 301);
+            this.pnlAcciones.Name = "pnlAcciones";
+            this.pnlAcciones.Size = new System.Drawing.Size(772, 41);
+            this.pnlAcciones.TabIndex = 15;
+            // 
+            // gbxLista
+            // 
+            this.gbxLista.Controls.Add(this.dgvLista);
+            this.gbxLista.Location = new System.Drawing.Point(12, 121);
+            this.gbxLista.Name = "gbxLista";
+            this.gbxLista.Size = new System.Drawing.Size(961, 174);
+            this.gbxLista.TabIndex = 16;
+            this.gbxLista.TabStop = false;
+            this.gbxLista.Text = "Lista Citas";
+            // 
+            // gbxDatos
+            // 
+            this.gbxDatos.Controls.Add(this.lblPaciente);
+            this.gbxDatos.Controls.Add(this.cbxPaciente);
+            this.gbxDatos.Controls.Add(this.btnCancelar);
+            this.gbxDatos.Controls.Add(this.btnGuardar);
+            this.gbxDatos.Controls.Add(this.nudPago);
+            this.gbxDatos.Controls.Add(this.lblPago);
+            this.gbxDatos.Controls.Add(this.dtpFechaResrvacion);
+            this.gbxDatos.Controls.Add(this.lblFechaReservacion);
+            this.gbxDatos.Controls.Add(this.txtMotivo);
+            this.gbxDatos.Controls.Add(this.lblMotivo);
+            this.gbxDatos.Location = new System.Drawing.Point(19, 369);
+            this.gbxDatos.Name = "gbxDatos";
+            this.gbxDatos.Size = new System.Drawing.Size(954, 183);
+            this.gbxDatos.TabIndex = 17;
+            this.gbxDatos.TabStop = false;
+            this.gbxDatos.Text = "Datos de la Cita";
+            // 
+            // nudPago
+            // 
+            this.nudPago.Location = new System.Drawing.Point(193, 123);
+            this.nudPago.Name = "nudPago";
+            this.nudPago.Size = new System.Drawing.Size(120, 27);
+            this.nudPago.TabIndex = 41;
+            // 
+            // lblPago
+            // 
+            this.lblPago.AutoSize = true;
+            this.lblPago.Location = new System.Drawing.Point(11, 125);
+            this.lblPago.Name = "lblPago";
+            this.lblPago.Size = new System.Drawing.Size(52, 20);
+            this.lblPago.TabIndex = 40;
+            this.lblPago.Text = "Pago:";
+            // 
+            // dtpFechaResrvacion
+            // 
+            this.dtpFechaResrvacion.Location = new System.Drawing.Point(193, 78);
+            this.dtpFechaResrvacion.Name = "dtpFechaResrvacion";
+            this.dtpFechaResrvacion.Size = new System.Drawing.Size(301, 27);
+            this.dtpFechaResrvacion.TabIndex = 39;
+            // 
+            // lblFechaReservacion
+            // 
+            this.lblFechaReservacion.AutoSize = true;
+            this.lblFechaReservacion.Location = new System.Drawing.Point(8, 83);
+            this.lblFechaReservacion.Name = "lblFechaReservacion";
+            this.lblFechaReservacion.Size = new System.Drawing.Size(158, 20);
+            this.lblFechaReservacion.TabIndex = 38;
+            this.lblFechaReservacion.Text = "Fecha Reservacion:";
+            // 
+            // txtMotivo
+            // 
+            this.txtMotivo.Location = new System.Drawing.Point(193, 35);
+            this.txtMotivo.Name = "txtMotivo";
+            this.txtMotivo.Size = new System.Drawing.Size(301, 27);
+            this.txtMotivo.TabIndex = 37;
+            // 
+            // lblMotivo
+            // 
+            this.lblMotivo.AutoSize = true;
+            this.lblMotivo.Location = new System.Drawing.Point(7, 38);
+            this.lblMotivo.Name = "lblMotivo";
+            this.lblMotivo.Size = new System.Drawing.Size(63, 20);
+            this.lblMotivo.TabIndex = 36;
+            this.lblMotivo.Text = "Motivo:";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(860, 145);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(87, 32);
+            this.btnCancelar.TabIndex = 43;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(763, 145);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(91, 32);
+            this.btnGuardar.TabIndex = 42;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // cbxPaciente
+            // 
+            this.cbxPaciente.FormattingEnabled = true;
+            this.cbxPaciente.Location = new System.Drawing.Point(678, 39);
+            this.cbxPaciente.Name = "cbxPaciente";
+            this.cbxPaciente.Size = new System.Drawing.Size(222, 28);
+            this.cbxPaciente.TabIndex = 44;
+            // 
+            // lblPaciente
+            // 
+            this.lblPaciente.AutoSize = true;
+            this.lblPaciente.Location = new System.Drawing.Point(589, 42);
+            this.lblPaciente.Name = "lblPaciente";
+            this.lblPaciente.Size = new System.Drawing.Size(79, 20);
+            this.lblPaciente.TabIndex = 45;
+            this.lblPaciente.Text = "Paciente:";
             // 
             // FrmCita
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 500);
-            this.Controls.Add(this.dgvLista);
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.ClientSize = new System.Drawing.Size(1000, 564);
+            this.Controls.Add(this.gbxDatos);
+            this.Controls.Add(this.gbxLista);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.lblBusqueda);
+            this.Controls.Add(this.txtParametro);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.pnlAcciones);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmCita";
-            this.Text = "::: Cita - Psiquiatrico :::";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmCita";
+            this.Load += new System.EventHandler(this.FrmCita_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).EndInit();
+            this.pnlAcciones.ResumeLayout(false);
+            this.gbxLista.ResumeLayout(false);
+            this.gbxDatos.ResumeLayout(false);
+            this.gbxDatos.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPago)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgvLista;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label lblBusqueda;
+        private System.Windows.Forms.TextBox txtParametro;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Panel pnlAcciones;
+        private System.Windows.Forms.GroupBox gbxLista;
+        private System.Windows.Forms.GroupBox gbxDatos;
+        private System.Windows.Forms.NumericUpDown nudPago;
+        private System.Windows.Forms.Label lblPago;
+        private System.Windows.Forms.DateTimePicker dtpFechaResrvacion;
+        private System.Windows.Forms.Label lblFechaReservacion;
+        private System.Windows.Forms.TextBox txtMotivo;
+        private System.Windows.Forms.Label lblMotivo;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label lblPaciente;
+        private System.Windows.Forms.ComboBox cbxPaciente;
     }
 }
--- Sis457Psiquiatrico/CpPsiquiatrico/FrmCita.cs
+++ Sis457Psiquiatrico/CpPsiquiatrico/FrmCita.cs
@@ -1,4 +1,5 @@
-﻿using ClnPsiquiatrico;
+﻿using CadPsiquiatrico;
+using ClnPsiquiatrico;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,10 +14,129 @@ namespace CpPsiquiatrico
 {
     public partial class FrmCita : Form
     {
+        bool esNuevo = false;
         public FrmCita()
         {
             InitializeComponent();
-            dgvLista.DataSource = CitaCln.listarPa("");
+            //dgvLista.DataSource = CitaCln.listarPa("");
+            // dgvLista.DataSource = PersonalCln.listarPa("");
+        }
+        private void listar()
+        {
+            var citas = CitaCln.listarPa(txtParametro.Text.Trim());
+            dgvLista.DataSource = citas;
+            dgvLista.Columns["id"].Visible = false;
+            dgvLista.Columns["idPaciente"].Visible = false;
+            dgvLista.Columns["estado"].Visible = false;
+            dgvLista.Columns["nombre_paciente"].HeaderText = "Paciente";
+            dgvLista.Columns["motivo_cita"].HeaderText = "Motivo_Cita";
+            dgvLista.Columns["fechaReservacion"].HeaderText = "Fecha_Reservacion";
+            dgvLista.Columns["pago"].HeaderText = "Pago_(Bs)";
+            dgvLista.Columns["usuarioRegistro"].HeaderText = "Usuario";
+            dgvLista.Columns["fechaRegistro"].HeaderText = "Fecha Registro";
+            btnEditar.Enabled = citas.Count > 0;
+            btnEliminar.Enabled = citas.Count > 0;
+            if (citas.Count > 0) dgvLista.Rows[0].Cells["motivo_cita"].Selected = true;
+        }
+        private void cargarPaciente ()
+        {
+            cbxPaciente.DataSource = PacienteCln.listar();
+            cbxPaciente.DisplayMember = "nombre";
+            cbxPaciente.ValueMember = "id";
+        }
+        private void FrmCita_Load(object sender, EventArgs e)
+        {
+            Size = new Size(1018, 400);
+            listar();
+            cargarPaciente();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            esNuevo = true;
+            Size = new Size(1018, 611);
+            txtMotivo.Focus();
+        }
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            esNuevo = false;
+            Size = new Size(1018, 611);
+
+            int index = dgvLista.CurrentCell.RowIndex;
+            int id = Convert.ToInt32(dgvLista.Rows[index].Cells["id"].Value);
+            var cita = CitaCln.get(id);
+            txtMotivo.Text = cita.motivo;
+            dtpFechaResrvacion.Value = cita.fechaReservacion;
+            nudPago.Value = cita.pago;
+        }
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Size = new Size(1018, 400);
+            limpiar();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            listar();
+        }
+
+        private void txtParametro_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter) listar();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            var cita = new Cita();
+            cita.motivo = txtMotivo.Text.Trim();
+            cita.fechaReservacion = dtpFechaResrvacion.Value;
+            cita.pago = nudPago.Value;
+            cita.usuarioRegistro = "FVC";
+
+            if (esNuevo)
+            {
+                cita.fechaRegistro = DateTime.Now;
+                cita.estado = 1;
+                cita.idPaciente = Convert.ToInt32(cbxPaciente.SelectedValue);
+                //cita.idPaciente = 1;
+                CitaCln.insertar(cita);
+            }
+            else
+            {
+                int index = dgvLista.CurrentCell.RowIndex;
+                cita.id = Convert.ToInt32(dgvLista.Rows[index].Cells["id"].Value);
+                CitaCln.actualizar(cita);
+            }
+            listar();
+            btnCancelar.PerformClick();
+            MessageBox.Show("Cita guardado exitosamente", "::: Psiquiatrico - Mensaje :::",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void limpiar()
+        {
+            txtMotivo.Text = string.Empty;
+            dtpFechaResrvacion.Value = DateTime.Now;
+            nudPago.Value = 0;
+        }
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            int index = dgvLista.CurrentCell.RowIndex;
+            int id = Convert.ToInt32(dgvLista.Rows[index].Cells["id"].Value);
+            string motivo_cita = dgvLista.Rows[index].Cells["motivo_cita"].Value.ToString();
+            DialogResult dialog = MessageBox.Show($"Esta seguro que desea eliminar la cita {motivo_cita}?",
+                "::: Psiquiatrico - Mensaje :::", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (dialog == DialogResult.OK)
+            {
+                CitaCln.eliminar(id, "FVC");
+                listar();
+                MessageBox.Show("Cita eliminada correctamente", "::: Psiquiatrico - Mensaje :::",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
     }
 }
